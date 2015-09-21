acd77a5ae18fa1643eabc487e35c3b7c0810b06c
Igor Kriklivetc <igor.kriklivets@hotmail.com>
2015-09-21 15:26:46 +0300

    Tests for sorting added

--- tests/queryBreezeTests.js
+++ tests/queryBreezeTests.js
@@ -6,22 +6,29 @@ $(function () {
         EntityQuery = breeze.EntityQuery,
         EntityManager = breeze.EntityManager;
 
+    var HTTP_STATUS_OK = 200;
+    var ODATA_V2_RESPONSE_HEADERS = {
+        "DataServiceVersion": 2.0,
+        "Content-Type": "application/json;charset=utf-8"
+    };
+
     breeze.config.initializeAdapterInstances({
         dataService: "OData"
     });
 
     function createBreezeQuery(options) {
         options = options || {};
-        return query(
-            options.manager || new EntityManager({
-                dataService: new DataService({
-                    serviceName: "...",
-                    hasServerMetadata: false
-                })
-            }),
-            options.resourceNameOrQuery || "resourceName",
-            options.queryOptions || {}
-            );
+
+        var resourceNameOrQuery = options.resourceNameOrQuery || "resourceName";
+        var queryOptions = options.queryOptions || {};
+        var manager = options.manager || new EntityManager({
+            dataService: new DataService({
+                serviceName: "...",
+                hasServerMetadata: false
+            })
+        });
+
+        return query(manager, resourceNameOrQuery, queryOptions);
     }
 
     QUnit.test("exists", function (assert) {
@@ -29,17 +36,14 @@ $(function () {
     });
 
     QUnit.test("enumerate", function (assert) {
-        var goFurther = assert.async();
+        var done = assert.async();
         var server = sinon.fakeServer.create({
             respondImmediately: true
         });
 
         server.respondWith([
-            200,
-            {
-                "DataServiceVersion": 2.0,
-                "Content-Type": "application/json;charset=utf-8"
-            },
+            HTTP_STATUS_OK,
+            ODATA_V2_RESPONSE_HEADERS,
             JSON.stringify({
                 d: {
                     results: [
@@ -50,7 +54,8 @@ $(function () {
             })
         ]);
 
-        createBreezeQuery().enumerate()
+        createBreezeQuery()
+            .enumerate()
             .fail(function () {
                 assert.ok(false, "Shouldn't reach this point");
             })
@@ -60,9 +65,30 @@ $(function () {
                     { "id": 2 }
                 ]);
                 assert.ok($.isEmptyObject(extra));
-            }).always(function () {
-                server.restore();
-                goFurther();
-            });
+            }).always($.proxy(server.restore, server), done);
+    });
+
+    QUnit.test("sortBy / thenBy", function (assert) {
+        var done = assert.async();
+        var server = sinon.fakeServer.create({
+            respondImmediately: true
+        });
+
+        server.respondWith(function (request) {
+            assert.ok(/a desc,b,c\/d/.test(decodeURIComponent(request.url)));
+        });
+
+        createBreezeQuery()
+            .sortBy("a", true)
+            .thenBy("b")
+            .thenBy("c.d")
+            .enumerate()
+            .always($.proxy(server.restore, server), done);
+    });
+
+    QUnit.test("thenBy cannot be called before sortBy", function (assert) {
+        assert.throws(function() {
+            createBreezeQuery.thenBy("b");
+        });
     });
 });
